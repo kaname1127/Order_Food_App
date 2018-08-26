f46cc34f2f987169e536de2e181e45bfa2c9aee2
廣田要 <hirotakaname@hirotakanames-MacBook-Pro.local>
2018-08-26 18:30:49 +0900

    20180926

---
+++ review.php
@@ -0,0 +1,30 @@
+<?php
+class Review {
+  private $menuName;
+  private $userId;
+  private $body;
+
+  public function __construct($menuName, $userId, $body) {
+    $this->userId = $userId;
+    $this->body = $body;
+  }
+
+  public function getMenuName() {
+    return $this->menuName;
+  }
+
+  public function getBody() {
+    return $this->body;
+  }
+  
+  public function getUser($users) {
+    foreach ($users as $user) {
+      if ($user->getId() == $this->userId) {
+        return $user;
+      }
+    }
+  }
+  
+}
+
+?>
