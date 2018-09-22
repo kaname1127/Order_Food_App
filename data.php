1b7e64557b049192eedb15eb41032bfd39848eb8
廣田要 <hirotakaname@hirotakanames-MacBook-Pro.local>
2018-09-22 15:20:22 +0900

    css修正

--- data.php
+++ data.php
@@ -18,14 +18,14 @@ $user4 = new User('sato', 'male');
 
 $users = array($user1, $user2, $user3, $user4);
 
-$review1 = new Review($juice->getName(), $user1->getId(), '果肉たっぷりのオレンジジュースです！');
-$review2 = new Review($curry->getName(), $user1->getId(), '具がゴロゴロしていてとてもおいしいです');
-$review3 = new Review($coffee->getName(), $user2->getId(), '香りがいいです');
-$review4 = new Review($pasta->getName(), $user2->getId(), 'ソースが絶品です。また食べたい。');
-$review5 = new Review($juice->getName(), $user3->getId(), '普通のジュース');
-$review6 = new Review($curry->getName(), $user3->getId(), '値段の割においしいカレーだと思いました');
-$review7 = new Review($coffee->getName(), $user4->getId(), '苦味がちょうどよくて、おすすめです');
-$review8 = new Review($pasta->getName(), $user4->getId(), '具材にこだわりを感じました。');
+$review1 = new Review($juice->getName(), $user1->getId(), 'ジュースてすと');
+$review2 = new Review($curry->getName(), $user1->getId(), 'カレーてすと');
+$review3 = new Review($coffee->getName(), $user2->getId(), 'コーヒーてすと');
+$review4 = new Review($pasta->getName(), $user2->getId(), 'パスタてすと');
+$review5 = new Review($juice->getName(), $user3->getId(), 'ジュースてすと２');
+$review6 = new Review($curry->getName(), $user3->getId(), 'カレーてすと２');
+$review7 = new Review($coffee->getName(), $user4->getId(), 'コーヒーてすと２');
+$review8 = new Review($pasta->getName(), $user4->getId(), 'パスタてすと２');
 
 $reviews = array($review1, $review2, $review3, $review4, $review5, $review6, $review7, $review8);
 
